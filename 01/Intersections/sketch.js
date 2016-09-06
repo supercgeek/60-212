87f14378eb0dbd56f55b66202b989e8267308f41
supercgeek <cameronbb3@gmail.com>
2016-09-06 19:58:13 -0400

    More Changes
    But, it’s still broken.

--- 01/Intersections/sketch.js
+++ 01/Intersections/sketch.js
@@ -2,11 +2,11 @@ function setup() {
   createCanvas(720, 480)
 }
 function mouseClicked() {
-  var coordinateArray_External = [];
+  // var coordinateArray_External = [];
   clear()
-  var coordinateArray_External = drawLine(coordinateArray_External);
-  // console.log(coordinateArray_External)
-  drawIntersections(coordinateArray_External)
+  drawLine();
+  // var coordinateArray_External = drawLine(coordinateArray_External);
+  // drawIntersections(coordinateArray_External)
 
 }
 
@@ -19,10 +19,10 @@ function drawLine(a) {
   for (var i = 1; i <= numberOfLines + 1; i++) {
     var aLine = {
       name: "Line #" + i,
-      randomX1: round(random(0, 720)),
-      randomY1: round(random(0, 480)),
-      randomX2: round(random(0, 720)),
-      randomY2: round(random(0, 480))
+      randomX1: round(random(0, width/2)),
+      randomY1: round(random(0, height)),
+      randomX2: round(random(width/2, width)),
+      randomY2: round(random(0, height))
     }
     // console.log("(" + aLine.randomX1 + ", " + aLine.randomY1 + ") TO (" + aLine.randomX2 + ", " + aLine.randomY2 + ")");
     line(aLine.randomX1, aLine.randomY1, aLine.randomX2, aLine.randomY2);
@@ -43,43 +43,55 @@ function drawLine(a) {
         x2: lineArray[k].randomX2,
         y1: lineArray[k].randomY1,
         y2: lineArray[k].randomY2,
+        slopeOfLine1: (this.y2-this.y1)/(this.x2-this.x1),
+        yIntOfLine1: twoLines.y1-(slopeOfLine1)*twoLines.x1,
+        yIntOfLine2: twoLines.y3-(slopeOfLine1)*twoLines.x3,
         
         // From Line Two
         x3: lineArray[n].randomX1,
         x4: lineArray[n].randomX2,
         y3: lineArray[n].randomY1,
         y4: lineArray[n].randomY2
-        }
-      
-      //INTERSETION CALCULATION
-      var uA = ((twoLines.x4-twoLines.x3)*(twoLines.y1-twoLines.y3)-(twoLines.y4-twoLines.x3)*(twoLines.x1-twoLines.x3))/((twoLines.y4-twoLines.y3)*(twoLines.x2-twoLines.x1)-(twoLines.x4-twoLines.x3)*(twoLines.y2-twoLines.y1))
-      var uB = ((twoLines.x2-twoLines.x1)*(twoLines.y1-twoLines.y3)-(twoLines.y2-twoLines.y1)*(twoLines.x1-twoLines.x3))/((twoLines.y4-twoLines.y3)*(twoLines.x2-twoLines.x1)-(twoLines.x4-twoLines.x3)*(twoLines.y2-twoLines.y1))
-      var xOfIntersection = twoLines.x1 + uA * (twoLines.x2 - twoLines.x1)
-      var yOfIntersection = twoLines.y1 + uA * (twoLines.y2 - twoLines.y1)
+        slopeOfLine2: (this.y3-this.y4)/(this.x3-this.x4)
         
-      var coordinatePair = {
-          coordinateId: twoLines.pairId,
-          xVal: xOfIntersection,
-          yVal: yOfIntersection
-        }
+      }
+      
+      //M
+      // var slopeOfLine1 = (twoLines.y2-twoLines.y1)/(twoLines.x2-twoLines.x1)
+      // var slopeOfLine2 = (twoLines.y3-twoLines.y4)/(twoLines.x3-twoLines.x4)
+      
+      //B
+      var yIntOfLine1 = twoLines.y1-(slopeOfLine1)*twoLines.x1
+      var yIntOfLine2 = twoLines.y3-(slopeOfLine1)*twoLines.x3
+    
+      //PAIRS
+      var xOfIntersection = (yIntOfLine1-yIntOfLine2)/(slopeOfLine2-slopeOfLine1)
+      var yOfIntersection = slopeOfLine1*xOfIntersection + yIntOfLine1;
+      if (xOfIntersection >= twoLines.x1 && xOfIntersection <= twoLines.x2) {
+        ellipse(xOfIntersection,yOfIntersection,5);
+      }
+      
+      // var coordinatePair = {
+      //     coordinateId: twoLines.pairId,
+      //     xVal: xOfIntersection,
+      //     yVal: yOfIntersection
+      //   }
         
       // console.log(coordinatePair);
-      coordinateArray_Internal.push(coordinatePair);
+      // coordinateArray_Internal.push(coordinatePair);
       }
     }
   }
-  return coordinateArray_Internal;
+  // return coordinateArray_Internal;
 }
 
-function drawIntersections (b) {
-  var coordinateArray_Internal = b;
-  for (var j = 0; j <= coordinateArray_Internal.length - 1; j++) {
-    var currentPairObject = coordinateArray_Internal[j];
-    // console.log("Obj: " + currentPairObject.xVal)
-    // console.log("j " + currentPairObject.xVal)
-    var x = round(currentPairObject.xVal)
-    var y = round(currentPairObject.yVal)
-    console.log("(" + x + ", " + y +")")
-    ellipse(x,y,5)
-  }  
-}
+// function drawIntersections (b) {
+//   var coordinateArray_Internal = b;
+//   for (var j = 0; j <= coordinateArray_Internal.length - 1; j++) {
+//     var currentPairObject = coordinateArray_Internal[j];
+//     var x = currentPairObject.xVal
+//     var y = currentPairObject.yVal
+//     println("(" + x + ", " + y +")")
+//     ellipse(x,y,5)
+//   }  
+// }
