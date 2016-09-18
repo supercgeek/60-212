fe8b3b2251ce69c4cd97b1680eff34038fdb905d
supercgeek <cameronbb3@gmail.com>
2016-09-18 04:38:13 -0400

    It Works

--- 02/cambu-clock/sketch.js
+++ 02/cambu-clock/sketch.js
@@ -3,15 +3,10 @@ var Y_AXIS = 1;
 var X_AXIS = 2;
 var c1, c2;
 var prevSec;
-var millisRolloverTime;
 
 function setup() {
-    colorMode(HSB, 100);
+    colorMode(RGB);
     createCanvas(750, 400);
-    millisRolloverTime = 0;
-    // Define colors
-    c1 = color(51,31,255); //72 alpha
-    c2 = color(0,0,0); //50 alpha
 }
 
 function draw() {
@@ -21,31 +16,34 @@ function draw() {
     var H = hour();
     var M = minute();
     var S = second();
-    H = 10;
-    console.log("Number of Hours: " + H)
+    // H = 23;
+    //var totalHourInMinSecs = M * 60; // this var out of 3600
+    var totalInNewUnits = newUnit(M, S);
+    // console.log(totalInNewUnits);
 
-    if (prevSec != S) {
-        millisRolloverTime = millis();
+    if (H <= 11) {
+        clockMode = "AM";
+
+        c1 = color(224, 182, 72); //,0.72); //72 alpha
+        c2 = color(0, 0, 0, 0.5); //50 alpha
+    } else {
+        clockMode = "PM";
+        H = H - 12;
+        c1 = color(51, 31, 255); //,0.72); //72 alpha
+        c2 = color(0, 0, 0, 0.5); //50 alpha
     }
-    prevSec = S;
-    var mils = floor(millis() - millisRolloverTime);
-
-    // fill(0, 0, 95);
-    // text("Hour: " + H, 10, 22);
+    fill(255, 255, 255);
+    //text("Hour: " + H, 10, 22);
     // text("Minute: " + M, 10, 42);
     // text("Second: " + S, 10, 62);
-    // text("Millis: " + mils, 10, 82);
-
-    //Call The Rectangle Creation Method
-    // stroke(0, 0, 100)
-    // line(0, 125, width, 125)
-    // line(0, 210, width, 210)
-    noStroke();
+    // // text("newUnits: " + newUnit, 10, 82);
+    // text("Millis: " + mils, 10, 102);
+    //text("Progress: " + totalInNewUnits + "/7225", 10, 120);
 
     var squareArray = [];
 
     //Create a Square & Push To Array
-    for (var i = 0; i < H; i++) {
+    for (var i = 0; i < H + 1; i++) {
         var presentSquare = new createSquare(i); //Sending i into the square constructor passes the current month into createSquare()
         squareArray.push(presentSquare);
     }
@@ -53,66 +51,85 @@ function draw() {
     //Add Placement Location to All Squares In Array
     squareArray = squarePlacement(squareArray);
 
-
-
-
-
-
-
     //Draw Squares
     for (var i = 0; i < H; i++) {
-        rect(squareArray[i].startX, squareArray[i].startY, squareArray[i].rectWidth, squareArray[i].rectHeight);
-        //          x   y    w    h   c2  c1   axis
         setGradient(squareArray[i].startX, squareArray[i].startY, squareArray[i].rectWidth, squareArray[i].rectHeight, c2, c1, X_AXIS);
+        // rect(squareArray[i].startX, squareArray[i].startY, squareArray[i].rectWidth, squareArray[i].rectHeight);
+        pointDraw(squareArray, H, totalInNewUnits);
     }
-
-    console.log("Things in the Array: " + squareArray.length);
-    noLoop();
+    // noLoop();
 }
 
+function pointDraw(squareArray, H, totalInNewUnits) {
+    stroke(255, 255, 255);
+    startXOfSquare = squareArray[H].startX;
+    startYOfSquare = squareArray[H].startY;
+    squarePlacer = squarePlacement(squareArray);
+    jumpValue = 85 + 20; //squarePlacer.jumpByValue;
+    newUnitIncrementor = 0;
+
+    for (var y = startYOfSquare-1; y <= startYOfSquare + 85; y++) {
+        for (var x = startXOfSquare; x <= startXOfSquare + 85; x++) {
+
+            if (newUnitIncrementor > totalInNewUnits) {
+                //DO NOTHING
+                // console.log("now break");
+            } else {
+                point(x, y);
+                newUnitIncrementor = newUnitIncrementor + 1;
+                // console.log("run");
+            }
+        }
+    }
+}
+function newUnit(M, S) {
+  // console.log(M + " " +  S);
+  var totalSecondsSinceStartOfHour = M * 60 + S;
+  totalSecondsSinceStartOfHour = map(totalSecondsSinceStartOfHour, 0, 3600, 0, 7225);
+  return round(totalSecondsSinceStartOfHour);
+}
 function squarePlacement(squareArray) {
-    console.log("Length of Array: " + squareArray.length);
+    //console.log("Length of Array: " + squareArray.length);
     //VALUES USED TO CALCULATE HORIZONTAL PLACEMENT
     squareWidth = squareArray[0].rectWidth; //The Width of Squares
     spaceBetweenSquares = 20; //The Amount of horizontal space between square
-    jumpByValue = squareWidth + spaceBetweenSquares; //The Amount of space resultant from the sum of a square and the space following a square
+    this.jumpByValue = squareWidth + spaceBetweenSquares; //The Amount of space resultant from the sum of a square and the space following a square
 
 
-    if (squareArray.length <= 6) {
+    if (squareArray.length <= -5) {
         squareDomain = (jumpByValue * squareArray.length) - spaceBetweenSquares; //The region of the page that includes the squares and the space between them
-        console.log("Case 1: Center Aligned + Expand Out");
+        //console.log("Case 1: Center Aligned + Expand Out");
         for (var k = 0; k < squareArray.length; k++) {
             squareArray[k].startX = (width - squareDomain) / 2 + (jumpByValue * k);
             squareArray[k].startY = (height - squareArray[k].rectHeight) / 2;
             // console.log(jumpByValue);
-            console.log(squareArray[k].startX);
+            //console.log(squareArray[k].startX);
         }
     } else { // Case2: Two Rows
-      squareDomain = (jumpByValue * 6) - spaceBetweenSquares;
+        squareDomain = (jumpByValue * 6) - spaceBetweenSquares;
 
         for (var k = 0; k < squareArray.length; k++) {
             if (k < 6) {
-                console.log("Case 2.1: Top Row + Expand From Left");
+                //console.log("Case 2.1: Top Row + Expand From Left");
                 squareHeight = squareArray[0].rectWidth;
                 spaceBelowSquares = 20;
                 jumpDownValue = squareHeight + spaceBelowSquares;
                 numberOfHours = squareArray.length;
-                console.log("numberOfHoursL " + numberOfHours);
+                //console.log("numberOfHoursL " + numberOfHours);
 
-                numOfRows = Math.ceil(numberOfHours/6),
-                console.log("Rows: " + numOfRows);
+                numOfRows = Math.ceil(numberOfHours / 6);
+                //console.log("Rows: " + numOfRows);
                 squareRange = (jumpDownValue * numOfRows) - spaceBelowSquares;
-                console.log("squareRange: "+ squareRange);
+                //console.log("squareRange: "+ squareRange);
 
                 // squareArray[k].startX = 130 + (jumpByValue * k);
                 squareArray[k].startX = (width - squareDomain) / 2 + (jumpByValue * k);
-                squareArray[k].startY = (height - squareRange) / 2 //+ (jumpDownValue * k);
+                squareArray[k].startY = (height - squareRange) / 2; //+ (jumpDownValue * k);
             } else { //7 to 12
-                console.log("Case 2.2: Second Row + Expand From Left");
+                //console.log("Case 2.2: Second Row + Expand From Left");
 
                 squareArray[k].startX = (width - squareDomain) / 2 + (jumpByValue * (k - 6));
-                // squareArray[k].startX = 130 + (jumpByValue * (k - 6));
-                squareArray[k].startY = (height - squareRange) / 2 + jumpDownValue // + (jumpDownValue * k);
+                squareArray[k].startY = (height - squareRange) / 2 + jumpDownValue; // + (jumpDownValue * k);
             }
         }
     }
@@ -120,7 +137,7 @@ function squarePlacement(squareArray) {
 }
 
 function createSquare(currentHourToCreate) {
-    var squareDimentions = 98; // this value is here because width * height must be the same
+    var squareDimentions = 85; // this value is here because width * height must be the same
     this.rectWidth = squareDimentions;
     this.rectHeight = squareDimentions;
     this.currentHour = currentHourToCreate;
@@ -128,22 +145,21 @@ function createSquare(currentHourToCreate) {
     this.yBegin = 0; //findYPos(this.currentHour, this.rectWidth, this.rectHeight);
 }
 
-function setGradient(x, y, w, h, c1, c2, axis) {//function from https://p5js.org/examples/examples/Color_Linear_Gradient.php
-  noFill();
-  if (axis == Y_AXIS) {  // Top to bottom gradient
-    for (var i = y; i <= y+h; i++) {
-      var inter = map(i, y, y+h, 0, 1);
-      var c = lerpColor(c1, c2, inter);
-      stroke(c);
-      line(x, i, x+w, i);
+function setGradient(x, y, w, h, c1, c2, axis) { //function from https://p5js.org/examples/examples/Color_Linear_Gradient.php
+    noFill();
+    if (axis == Y_AXIS) { // Top to bottom gradient
+        for (var i = y; i <= y + h; i++) {
+            var inter = map(i, y, y + h, 0, 1);
+            var c = lerpColor(c1, c2, inter);
+            stroke(c);
+            line(x, i, x + w, i);
+        }
+    } else if (axis == X_AXIS) { // Left to right gradient
+        for (var i = x; i <= x + w; i++) {
+            var inter = map(i, x, x + w, 0, 1);
+            var c = lerpColor(c1, c2, inter);
+            stroke(c);
+            line(i, y, i, y + h);
+        }
     }
-  }
-  else if (axis == X_AXIS) {  // Left to right gradient
-    for (var i = x; i <= x+w; i++) {
-      var inter = map(i, x, x+w, 0, 1);
-      var c = lerpColor(c1, c2, inter);
-      stroke(c);
-      line(i, y, i, y+h);
-    }
-  }
 }
