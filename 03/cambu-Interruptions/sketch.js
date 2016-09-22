eac11eadf20050219631cc0935f0291868c46f9a
supercgeek <cameronbb3@gmail.com>
2016-09-21 22:26:22 -0400

    Continued

--- 03/cambu-Interruptions/sketch.js
+++ 03/cambu-Interruptions/sketch.js
@@ -1,9 +1,10 @@
 var newDomain; // a global declaration
 var newRange;
-var spaceValue = 15;
+var spaceValue = 13;
 
 function setup() {
     createCanvas(750, 750);
+    background(244, 244, 244);
     newDomain = width * 0.8; // an assignment (of a global variable)
     newRange = height * 0.8;
 }
@@ -19,8 +20,9 @@ function draw() {
 }
 
 function mousePressed() {
+  background(244, 244, 244);
     //Generate a new drawing whenever the user clicks
-    background(255, 255, 255);
+
     console.log(newDomain);
     interruptor(newDomain, newRange, spaceValue);
 }
@@ -33,8 +35,6 @@ function interruptor(newDomain, newRange, spaceValue) {
   for (var y = startY; y < newRange + startY; y = y + spaceValue) {//Y = constant,
     for (var x = startX; x < newDomain + startX; x = x + spaceValue) {//itterate X
       randomLine(x, y);
-      // ellipse(x, y, 2, 2);
-      point(x, y);
     }
   }
 
@@ -42,9 +42,24 @@ function interruptor(newDomain, newRange, spaceValue) {
 
 function randomLine(x1, y1) {
   //calculate a length in a random direction of 50px
-  var length = 15;
-  var angle = random(0,360);
+  var length = 20;
+  var randomSeed = random(0.05, 0.95)
+  var biasedRandom = ExponentialSmoothedStaircase (randomSeed,0, 5);
+  var angle = (biasedRandom * TWO_PI) + radians(92); //angle in radians
+  console.log(angle);
   var x2 = x1 + length * cos(angle);
   var y2 = y1 + length * sin(angle);
   line(x1, y1, x2, y2);
+}
+
+function ExponentialSmoothedStaircase ( x,  a,  n) {
+   // See http://web.mit.edu/fnl/volume/204/winston.html
+
+  var fa = sq (map(a, 0,1, 5,30));
+  var y = 0;
+  for (var i=0; i<n; i++){
+    y += (1.0/(n-1.0))/ (1.0 + exp(fa*(((i+1.0)/n) - x)));
+  }
+  y = constrain(y, 0,1);
+  return y;
 }
